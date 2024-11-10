143e4b8b4362a48973960d215f0f48d4efdaf53e
Kozlov_S_S <asdfffdw@gmail.com>
2024-11-10 19:47:29 +0300

    refactor code

--- Container/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.container","l":"Container"},{"p":"org.container","l":"ContainerNode"},{"p":"org.container","l":"ContainerRunner"}];updateSearchResults();
